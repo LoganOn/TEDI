6f57926a0ec16c32cb12d9720c5a52a5db026656
Konrad Krawczyk <k.krawczyk@femax.pl>
2020-12-22 17:55:30 +0100

    Created a few tables

--- docs/database/create-tedi-db.sql
+++ docs/database/create-tedi-db.sql
@@ -7,8 +7,8 @@ FLUSH PRIVILEGES;
 USE tedi_db;
 ALTER DATABASE server_db CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS `Users` (
-  `UserId` Integer NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Customers` (
+  `CustomerId` Integer NOT NULL AUTO_INCREMENT,
   `Role` Varchar(20),
   `Name` Varchar(32),
   `Phone` Varchar(20),
@@ -19,5 +19,74 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `providerId` Varchar(255),
   `CreationDate` Timestamp NOT NULL,
   `UserVerified` boolean,
-  PRIMARY KEY (`UserId`)
+  PRIMARY KEY (`CustomerId`)
 );
+
+CREATE TABLE IF NOT EXISTS `Suppliers` (
+  `SupplierId` Integer NOT NULL AUTO_INCREMENT,
+  `Role` Varchar(20),
+  `Name` Varchar(32),
+  `Phone` Varchar(20),
+  `Email` Varchar(64) NOT NULL UNIQUE,
+  `Password` Varchar(64) NOT NULL,
+  `imageUrl` MEDIUMTEXT,
+  `provider` Varchar(255),
+  `providerId` Varchar(255),
+  `CreationDate` Timestamp NOT NULL,
+  `UserVerified` boolean,
+  PRIMARY KEY (`SupplierId`)
+);
+
+CREATE TABLE IF NOT EXISTS `Relations` (
+  `RelationId` Integer NOT NULL AUTO_INCREMENT,
+  `SupplierId` Varchar(20),
+  `CustomerId` Varchar(32),
+  `Active` boolean,
+  `CreationDate` Timestamp NOT NULL,
+  PRIMARY KEY (`RelationId`),
+  FOREIGN KEY (`SupplierId`)
+    REFERENCES Suppliers(SupplierId),
+  FOREIGN KEY (`CustomerId`)
+    REFERENCES Customers(CustomerId)
+
+);
+
+CREATE TABLE IF NOT EXISTS `DeliveryOrders` (
+  `DeliveryOrderId` Integer NOT NULL AUTO_INCREMENT,
+  `DocNumber` Integer NOT NULL,
+  `DocStatus` Varchar(1) NOT NULL , -- close, open, cancelled
+  `SupplierId` Integer,
+  `UserId` Integer,
+  `CreationDate` Timestamp NOT NULL,
+  `DocTotal` Numeric(10,2),  -- total value order
+  `DocNet` Numeric(10,2), -- net value
+  `DocVatSum` Numeric(10,2), -- tax value
+  `DocNumberPosition` Integer,
+  PRIMARY KEY (`RelationId`)
+);
+
+CREATE TABLE IF NOT EXISTS `DetailsDeliveryOrders` (
+  `DetailsDeliveryOrderId` Integer NOT NULL AUTO_INCREMENT, --tworzonu z DocNumber w tabeli DeliveryOrders
+  `LineNum` Integer NOT NULL,
+  `DocNumber` Integer NOT NULL,
+  `SupplierId` Varchar(20),
+  `UserId` Varchar(32),
+  `ItemCode` Varchar(100),
+  `ItemName` Varchar(255),
+  `Quantity` Numeric(10,2),
+  `CodeBars` Varchar(13),
+  `ShipDate` Timestamp NOT NULL,
+  `Price` Numeric(10,2),
+  `Currency` Varchar(10),
+  `LineTotal` Numeric(10,2),
+  `LineVat` Numeric(10,2),
+  `VatPrcnt` Numeric(10,2),
+  `VatGroup` Varchar(10),
+  `Active` boolean,
+  `CreationDate` Timestamp NOT NULL,
+  PRIMARY KEY (`DetailsDeliveryOrderId`)
+  FOREIGN KEY (`DocNumber`)
+    REFERENCES DeliveryOrders(DocNumber),
+);
+
+
