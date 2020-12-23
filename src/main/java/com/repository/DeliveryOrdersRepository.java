9fe4dc3f55eff93a8ece6eb939d210c25c62db6e
Konrad Krawczyk <k.krawczyk@femax.pl>
2020-12-23 17:27:17 +0100

    disabled login form in Spring Security

--- src/main/java/com/repository/DeliveryOrdersRepository.java
+++ src/main/java/com/repository/DeliveryOrdersRepository.java
@@ -1,6 +1,7 @@
 package com.repository;
 
+import com.model.DeliveryOrders;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DeliveryOrdersRepository extends CrudRepository<DeliveryOrdersRepository, Long> {
+public interface DeliveryOrdersRepository extends CrudRepository<DeliveryOrders, Long> {
 }
