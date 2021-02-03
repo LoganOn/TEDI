10884834b5efb11f67220296eab1beb0b1250264
Konrad Krawczyk <k.krawczyk@femax.pl>
2021-02-03 17:11:39 +0100

    s

--- src/main/java/com/controller/DeliveryOrdersController.java
+++ src/main/java/com/controller/DeliveryOrdersController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import com.model.DeliveryOrders;
+import com.model.Users;
 import com.repository.DeliveryOrdersRepository;
 import com.repository.UsersRepository;
 import lombok.AllArgsConstructor;
@@ -19,72 +20,79 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class DeliveryOrdersController {
 
-    private final DeliveryOrdersRepository deliveryOrdersRepository;
+  private final DeliveryOrdersRepository deliveryOrdersRepository;
 
-    private final UsersRepository usersRepository;
+  private final UsersRepository usersRepository;
 
-    private static final int SIZE = 20;
+  private static final int SIZE = 20;
 
-    @GetMapping
-    public ResponseEntity<?> findAllOrders(@RequestParam(required = false) Integer page, @RequestParam(required = false) Integer size) {
-        int p = page == null ?  1 : page <= 0 ? 1 : page;
-        int s = size == null ?  1 : size <= 20 ? SIZE : size;
-        List<DeliveryOrders> deliveryOrders = (List<DeliveryOrders>) deliveryOrdersRepository.findAll(PageRequest.of(p - 1,s));
-        return new ResponseEntity<>(
-                deliveryOrders, deliveryOrders == null ?
-                HttpStatus.NOT_FOUND : deliveryOrders.isEmpty() ?
-                HttpStatus.NO_CONTENT : HttpStatus.OK
-        );
+  @GetMapping
+  public ResponseEntity<?> findAllOrders(@RequestParam(required = false) Integer page, @RequestParam(required = false) Integer size) {
+    int p = page == null ? 1 : page <= 0 ? 1 : page;
+    int s = size == null ? 1 : size <= 20 ? SIZE : size;
+    List<DeliveryOrders> deliveryOrders = (List<DeliveryOrders>) deliveryOrdersRepository.findAll(PageRequest.of(p - 1, s));
+    return new ResponseEntity<>(
+            deliveryOrders, deliveryOrders == null ?
+            HttpStatus.NOT_FOUND : deliveryOrders.isEmpty() ?
+            HttpStatus.NO_CONTENT : HttpStatus.OK
+    );
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> findOrdersById(@PathVariable Long id) {
+    Optional<DeliveryOrders> deliveryOrder = deliveryOrdersRepository.findById(id);
+    return new ResponseEntity<>(
+            deliveryOrder, deliveryOrder == null ?
+            HttpStatus.NOT_FOUND : deliveryOrder.isEmpty() ?
+            HttpStatus.NO_CONTENT : HttpStatus.OK
+    );
+  }
+
+  @GetMapping("/supplier/{id}")
+  public ResponseEntity<?> findAllOrdersBySupplierId(@PathVariable Long id) {
+    List<DeliveryOrders> deliveryOrders = deliveryOrdersRepository.findAllByUserId1(usersRepository.findById(id).get().getUserId());
+    return new ResponseEntity<>(
+            deliveryOrders, deliveryOrders == null ?
+            HttpStatus.NOT_FOUND : deliveryOrders.isEmpty() ?
+            HttpStatus.NO_CONTENT : HttpStatus.OK
+    );
+  }
+
+  @GetMapping("/customer/{id}")
+  public ResponseEntity<?> findAllOrdersByCustomerId(@PathVariable Long id) {
+    List<DeliveryOrders> deliveryOrders = deliveryOrdersRepository.findAllByUserId2(usersRepository.findById(id).get().getUserId());
+    return new ResponseEntity<>(
+            deliveryOrders, deliveryOrders == null ?
+            HttpStatus.NOT_FOUND : deliveryOrders.isEmpty() ?
+            HttpStatus.NO_CONTENT : HttpStatus.OK
+    );
+  }
+
+  @GetMapping("/customer/{id1}/supplier/{id2}")
+  public ResponseEntity<?> findAllOrdersByCustomerIdAndSupplierId(@PathVariable Long id1, @PathVariable Long id2) {
+    List<DeliveryOrders> deliveryOrders = deliveryOrdersRepository.findAllByUserId2(usersRepository.findById(id1).get().getUserId());
+    List<DeliveryOrders> deliveryOrdersFiltering = deliveryOrders.stream().filter(d -> d.getUserId1() == id2).collect(Collectors.toList());
+    return new ResponseEntity<>(
+            deliveryOrdersFiltering, deliveryOrdersFiltering == null ?
+            HttpStatus.NOT_FOUND : deliveryOrdersFiltering.isEmpty() ?
+            HttpStatus.NO_CONTENT : HttpStatus.OK
+    );
+  }
+
+  //TODO change to headers
+  @GetMapping
+  public ResponseEntity<?> findOrdersByUserandBaseRefandCustomerNumber(@RequestParam(required = false, defaultValue = "") String name,
+                                                                       @RequestParam(required = false, defaultValue = "") String baseRef,
+                                                                       @RequestParam(required = false, defaultValue = "") String CusNumber) {
+    List<DeliveryOrders> deliveryOrders;
+    if(!name.isBlank()) {
+      Users users = usersRepository.fin
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> findOrdersById(@PathVariable Long id) {
-        Optional<DeliveryOrders> deliveryOrder = deliveryOrdersRepository.findById(id);
-        return new ResponseEntity<>(
-                deliveryOrder, deliveryOrder == null ?
-                HttpStatus.NOT_FOUND : deliveryOrder.isEmpty() ?
-                HttpStatus.NO_CONTENT : HttpStatus.OK
-        );
-    }
-
-    @GetMapping("/supplier/{id}")
-    public ResponseEntity<?> findAllOrdersBySupplierId(@PathVariable Long id) {
-        List<DeliveryOrders> deliveryOrders = deliveryOrdersRepository.findAllByUserId1(usersRepository.findById(id).get().getUserId());
-        return new ResponseEntity<>(
-                deliveryOrders, deliveryOrders == null ?
-                HttpStatus.NOT_FOUND : deliveryOrders.isEmpty() ?
-                HttpStatus.NO_CONTENT : HttpStatus.OK
-        );
-    }
-
-    @GetMapping("/customer/{id}")
-    public ResponseEntity<?> findAllOrdersByCustomerId(@PathVariable Long id) {
-        List<DeliveryOrders> deliveryOrders = deliveryOrdersRepository.findAllByUserId2(usersRepository.findById(id).get().getUserId());
-        return new ResponseEntity<>(
-                deliveryOrders, deliveryOrders == null ?
-                HttpStatus.NOT_FOUND : deliveryOrders.isEmpty() ?
-                HttpStatus.NO_CONTENT : HttpStatus.OK
-        );
-    }
-
-    @GetMapping("/customer/{id1}/supplier/{id2}")
-    public ResponseEntity<?> findAllOrdersByCustomerIdAndSupplierId(@PathVariable Long id1, @PathVariable Long id2) {
-        List<DeliveryOrders> deliveryOrders = deliveryOrdersRepository.findAllByUserId2(usersRepository.findById(id1).get().getUserId());
-        List<DeliveryOrders> deliveryOrdersFiltering = deliveryOrders.stream().filter(d-> d.getUserId1() == id2).collect(Collectors.toList());
-        return new ResponseEntity<>(
-                deliveryOrdersFiltering, deliveryOrdersFiltering == null ?
-                HttpStatus.NOT_FOUND : deliveryOrdersFiltering.isEmpty() ?
-                HttpStatus.NO_CONTENT : HttpStatus.OK
-        );
-    }
-    //TODO change to headers
-//    @GetMapping("/{baseRef}")
-//    public ResponseEntity<?> findOrderByBaseRef(@PathVariable String baseRef) {
-//        List<DeliveryOrders> deliveryOrders = deliveryOrdersRepository.findByBaseRefContaining(baseRef);
-//        return new ResponseEntity<>(
-//                deliveryOrders, deliveryOrders == null ?
-//                HttpStatus.NOT_FOUND : deliveryOrders.isEmpty() ?
-//                HttpStatus.NO_CONTENT : HttpStatus.OK
-//        );
-
+    List<DeliveryOrders> deliveryOrders = deliveryOrdersRepository.findByBaseRefContaining(baseRef);
+    return new ResponseEntity<>(
+            deliveryOrders, deliveryOrders == null ?
+            HttpStatus.NOT_FOUND : deliveryOrders.isEmpty() ?
+            HttpStatus.NO_CONTENT : HttpStatus.OK
+    );
+  }
 }
