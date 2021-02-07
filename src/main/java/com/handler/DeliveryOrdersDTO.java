d90bfc5e4292089861f6158b8284fcc6458311b8
Konrad Krawczyk <konrad.krawczyk@mayeryn.com>
2021-02-07 14:43:03 +0100

    first implementation

--- src/main/java/com/handler/DeliveryOrdersDTO.java
+++ src/main/java/com/handler/DeliveryOrdersDTO.java
@@ -1,13 +1,20 @@
 package com.handler;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.model.DetailsDeliveryOrders;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
 import java.sql.Timestamp;
+import java.util.List;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
+@ToString
 public class DeliveryOrdersDTO {
 
   private Long deliveryOrderId;
@@ -33,6 +40,8 @@ public class DeliveryOrdersDTO {
   private Timestamp creationDate;
 
   private Timestamp modifyDate;
+  @JsonIgnore
+  private List<DetailsDeliveryOrders> detailsDeliveryOrdersList;
 }
 
 
