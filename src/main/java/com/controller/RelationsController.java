6188138da7c19f6aa4be67f9bb7b19ad426663f4
Konrad Krawczyk <konrad.krawczyk@mayeryn.com>
2021-02-10 18:20:36 +0100

    added deleted method to RelationsController

--- src/main/java/com/controller/RelationsController.java
+++ src/main/java/com/controller/RelationsController.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import com.exception.ResourceNotFoundException;
 import com.model.RelationsUsers;
 import com.repository.RelationsUsersRepository;
 import com.repository.UsersRepository;
@@ -7,17 +8,17 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/relations", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class RelationsController {
+
+    private final String RESOURCE_NOT_FOUND= "Resource not found";
 
     private RelationsUsersRepository relationsRepository;
 
@@ -49,6 +50,20 @@ public class RelationsController {
         return new ResponseEntity<>(
                 relationsList, relationsList == null ?
                 HttpStatus.NOT_FOUND : relationsList.isEmpty() ?
+                HttpStatus.NO_CONTENT : HttpStatus.OK
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Optional<RelationsUsers> optionalRelationsUsers = relationsRepository.findById(id);
+        if(optionalRelationsUsers.isEmpty()){
+            throw new ResourceNotFoundException(RESOURCE_NOT_FOUND);
+        }
+        relationsRepository.delete(optionalRelationsUsers.get());
+        return new ResponseEntity<>(
+                optionalRelationsUsers.get().getRelationUsersId(), optionalRelationsUsers.isEmpty() ?
+                HttpStatus.NOT_FOUND : optionalRelationsUsers.isEmpty() ?
                 HttpStatus.NO_CONTENT : HttpStatus.OK
         );
     }
