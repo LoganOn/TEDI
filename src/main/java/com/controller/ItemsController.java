50a5868accebc095e22ceb6d8aa198b7f235694b
Konrad Krawczyk <k.krawczyk@femax.pl>
2021-02-25 17:43:01 +0100

    created controller

--- src/main/java/com/controller/ItemsController.java
+++ src/main/java/com/controller/ItemsController.java
@@ -1,26 +1,70 @@
 package com.controller;
 
+import com.exception.BadRequestException;
+import com.exception.ResourceNotFoundException;
+import com.handler.DeliveryOrdersDTO;
 import com.model.DeliveryOrders;
+import com.model.Items;
+import com.model.Users;
+import com.repository.ItemsRepository;
+import com.service.ItemService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.mail.FetchProfile;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/api/delivery", produces = "application/json")
+@RequestMapping(value = "/api/items", produces = "application/json")
 @AllArgsConstructor
 public class ItemsController {
 
-  @GetMapping("/{id}")
-  public ResponseEntity<?> findOrdersById(@PathVariable Long id) {
-    Optional<DeliveryOrders> deliveryOrder = deliveryOrdersRepository.findById(id);
+  private final String RESOURCE_NOT_FOUND = "Resource not found";
+
+  private final ItemsRepository itemsRepository;
+
+  private final ItemService itemService;
+
+  @GetMapping
+  public ResponseEntity<?> findAllItems() {
+    List<Items> items = itemsRepository.findAll();
     return new ResponseEntity<>(
-            deliveryOrder, deliveryOrder.isEmpty() ?
+            items, items.isEmpty() ?
+            HttpStatus.NO_CONTENT : HttpStatus.OK
+    );
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> findItemById(@PathVariable Long id) {
+    Optional<Items> items = itemsRepository.findById(id);
+    return new ResponseEntity<>(
+            items, items.isEmpty() ?
+            HttpStatus.NO_CONTENT : HttpStatus.OK
+    );
+  }
+
+  @PostMapping(consumes = "application/json")
+  public ResponseEntity addItems(@RequestBody Items items) {
+    Integer id = itemsRepository.findAll().size() + 1;
+    itemService.save(items);
+    return new ResponseEntity<>(
+            id, id == 0 ?
+            HttpStatus.NO_CONTENT : HttpStatus.OK
+    );
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> deleteItems(@PathVariable Long id) {
+    Optional<Items> items = itemsRepository.findById(id);
+    if (items.isEmpty()) {
+      throw new ResourceNotFoundException(RESOURCE_NOT_FOUND);
+    }
+    itemService.delete(items.get());
+    return new ResponseEntity<>(
+            items, items.isEmpty() ?
             HttpStatus.NO_CONTENT : HttpStatus.OK
     );
   }
--- src/main/java/com/repository/ItemsRepository.java
+++ src/main/java/com/repository/ItemsRepository.java
@@ -4,6 +4,10 @@ import com.model.Items;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemsRepository extends CrudRepository<Items, Long> {
+
+  List<Items> findAll();
 }
